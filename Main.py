fc17766949ccc3cbda722686245a512686a83528
0034120 <54878851+0034120@users.noreply.github.com>
2022-03-26 10:06:23 +0800

    Change Main.py to allow terminal running using args

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import easygui
+import argparse
+import os
+import sys
 import B64B64Rotcipher as bbr
 import B64B64RotcipherDECODE as bbrd
 
@@ -12,5 +15,35 @@ def main():
 		if c == "Quit":
 			break
 
+def main2(msg, key, Encrypt=True):
+	if Encrypt:
+		pass
+
 if __name__ == '__main__':
-	main()
+	my_parser = argparse.ArgumentParser(description="Encrypt or decrypt some text according to a key")
+
+	# add the arguments
+	my_parser.add_argument("msg",
+							metavar='Must supply a message to encrypt',
+							type=bool,
+							help='the message to encrypt')
+	my_parser.add_argument("key",
+							metavar="Must supply a key for encryption for better security",
+							type=str,
+							help='the key to encrypt the message')
+	args = my_parser.parse_args()
+
+	key = args.key
+	msg = args.msg
+
+	isencs = str(input("Is it [e]ncrypt or [d]ecrypt?\n\n- "))
+
+	if isencs.lower() == 'e':
+		isenc = True
+	if isencs.lower() == 'd':
+		isenc = False
+	if isencs.lower() != 'e' or 'd':
+		quit()
+
+
+	main2(msg, key, isenc)
