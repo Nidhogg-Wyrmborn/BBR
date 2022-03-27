3144ccd44100cd60bb61021713e79940ef398c19
0034120 <54878851+0034120@users.noreply.github.com>
2022-03-27 09:47:47 +0800

    Testing File Encryption

--- Main.py
+++ Main.py
@@ -15,11 +15,37 @@ def main():
 		if c == "Quit":
 			break
 
-def main2(msg, key, Encrypt=True):
+def main2(msg, key, Encrypt, isfile):
 	if Encrypt:
-		return(bbr.btwc(msg, key))
+		if not isfile:
+			return(bbr.btwc(msg, key))
+		if isfile:
+			with open(msg, 'r') as file:
+				rl = file.readlines()
+			
+			rs = ''
+			for i in rl:
+				rs = rs + i
+
+			with open(msg+".bbr", 'w') as file:
+				file.write(bbr.btwc(rs, key))
+
+			return f"encrypted file is {msg}.bbr"
+
 	if not Encrypt:
-		return(bbrd.decode(msg, key))
+		if not isfile:
+			return(bbrd.decode(msg, key))
+		if isfile:
+			with open(msg, 'r') as file:
+				rl = file.readlines()
+
+			rs = ''
+			for i in rl:
+				rs = rs + i
+
+			with open(msg.replace(".bbr", ''), 'w') as file:
+				file.write(bbrd.decode(rs, key))
+			return f"decrypted file is {msg.replace('.bbr','')}"
 
 if __name__ == '__main__':
 	my_parser = argparse.ArgumentParser(description="Encrypt or decrypt some text according to a key")
@@ -47,14 +73,19 @@ if __name__ == '__main__':
 							action='store_false',
 							dest='isencs',
 							help='Decrypt otherwise encrypt (if not called)')
+	my_parser.add_argument("-f", "--file",
+							action='store_true',
+							dest='isfile',
+							help='Is not file unless this argument is called, if this argument is called -m must be a filepath')
 	args = my_parser.parse_args()
 
 	msg = args.msg
 	key = args.key
 	isencs = args.isencs
+	isfile = args.isfile
 
 	if msg == None:
 		quit()
 
 
-	print(main2(msg, key, isencs))
+	print(main2(msg, key, isencs, isfile))
