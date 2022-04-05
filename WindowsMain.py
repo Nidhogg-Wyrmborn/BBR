ddc6427069fa9018ce5d1db27f8cff27682a3a4a
Nidhogg-Wyrmborn <0034120@jwacs.wa.edu.au>
2022-04-05 09:37:42 +0800

    WIP

--- WindowsMain.py
+++ WindowsMain.py
@@ -4,8 +4,25 @@ import B64B64Rotcipher as bbr
 import tarfile
 import os
 import sys
+from tqdm import tqdm
 from tkinter import *
 import easygui
+
+def compresstree(tar_file, members):
+    os.mkdir(f"./{tar_file.replace('.tar.gz','')}")
+    for i in range(len(members)):
+        members[i] = members[i].replace("\\", "/")
+        
+    for i in members:
+        with open(i, 'rb') as file:
+            with open(f"./{tar_file.replace('.tar.gz','')}/{i.split('/')[len(i.split('/'))-1]}", 'wb') as file2write:
+                fl = file.readlines()
+                fs = b''.join(fl)
+                file2write.write(fs)
+    for i in range(len(members)):
+        members[i] = f"./{tar_file.replace('.tar.gz','')}/{members[i].split('/')[len(members[i].split('/'))-1]}"
+    compress(tar_file,members)
+
 
 def compress(tar_file, members):
     """
@@ -45,35 +62,57 @@ def decompress(tar_file, path, members=None):
     tar.close()
 
 def encrypt(msg, key):
-	return bbr.btwc(msg, key)
+    return bbr.btwc(msg, key)
 
 def decrypt(msg, key):
-	pass
+    return bbrd.decode(msg, key)
 
 def efile(fpath, spath, key):
-	pass
+    try:
+        print(fpath)
+        if len(fpath)==1:
+            fpath = fpath[0]
+            compressed = False
+        elif len(fpath)>1:
+            compresstree("CompressedTree.tar.gz", fpath)
+            fpath = "CompressedTree.tar.gz"
+            compressed = True
+        with open(fpath, 'rb') as file:
+            fl = file.readlines()
+
+        fs = b''.join(fl)
+
+        if compressed:        
+            with open(spath+".tar.gz.bbr", 'wb') as file:
+                file.write(bbr.btwc(fs, key))
+        elif not compressed:
+            with open(spath, 'wb') as file:
+                file.write(bbr.btwc(fs, key))
+    except KeyboardInterrupt:
+        print("User Interrupt")
+        print("Removing Temporary files")
 
 def dfile(fpath, key):
-	pass
+    pass
 
 def main():
-	running = True
-	while running:
-		c = easygui.buttonbox("Encrypt Or Decrypt?", choices=['Encrypt','Encrypt File','Decrypt','Decrypt File','Quit'])
-		if c == "Quit":
-			running = False
+    running = True
+    while running:
+        c = easygui.buttonbox("Encrypt Or Decrypt?", choices=['Encrypt','Encrypt File','Decrypt','Decrypt File','Quit'])
+        if c == "Quit":
+            running = False
 
-		if c == "Encrypt":
-			easygui.msgbox(encrypt(easygui.enterbox("Message to encrypt:"),easygui.enterbox("Passcode:")))
+        if c == "Encrypt":
+            easygui.msgbox(encrypt(easygui.enterbox("Message to encrypt:"),easygui.enterbox("Passcode:")))
 
-		if c == "Encrypt File":
-			efile(easygui.fileopenbox(multiple=True), easygui.filesavebox(), easygui.enterbox("Passcode:"))
+        if c == "Encrypt File":
+            efile(easygui.fileopenbox(multiple=True), easygui.filesavebox(), easygui.enterbox("Passcode:"))
 
-		if c == "Decrypt":
-			easygui.msgbox(decrypt(easygui.enterbox("Message to decrypt:"),easygui.enterbox("Passcode:")))
+        if c == "Decrypt":
+            easygui.msgbox(decrypt(easygui.enterbox("Message to decrypt:"),easygui.enterbox("Passcode:")))
 
-		if c == "Decrypt File":
-			dfile(easygui.fileopenbox(multiple=False),easygui.enterbox("Passcode:"))
+        if c == "Decrypt File":
+            dfile(easygui.fileopenbox(multiple=False),easygui.enterbox("Passcode:"))
 
 if __name__ == '__main__':
-	main()
+    main()
